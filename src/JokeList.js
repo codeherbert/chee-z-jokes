4c1fef2b8f784c4f06576c5af9f9efc38c1afb37
tech master <bryan77@twc.com>
2020-06-30 08:03:17 -0400

    create multiple jokes and set state

--- src/JokeList.js
+++ src/JokeList.js
@@ -2,12 +2,24 @@ import React, {Component} from "react";
 import axios from "axios";
 
 class JokeList extends Component {
+    static defaultProps = {
+        numJokesToGet: 10
+    };
+    constructor(props) {
+        super(props);
+        this.state = { jokes: [] }
+    };
+
     async componentDidMount() {
-        // load jokes
-        let res = await axios.get("https://icanhazdadjoke.com/", {
-            headers: { Accept : "application/json" }
-        });
-        console.log(res.data.joke);
+        let jokes = [];
+        while(jokes.length < this.props.numJokesToGet) {
+            // load jokes
+            let res = await axios.get("https://icanhazdadjoke.com/", {
+                headers: { Accept : "application/json" }
+            });
+            jokes.push(res.data.joke);
+        }
+        this.setState({ jokes : jokes});
     }
     render() {
         return (
